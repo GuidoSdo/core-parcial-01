0ee4eb8d8112af29b3263bcc5a16f9adb57d962a
Nicoo <nicoo.bernstein@gmail.com>
2025-09-22 17:25:42 -0300

    feat: sonidos de pasos

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,7 @@
 using System.ComponentModel;
-using Unity.Mathematics;
 using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float RunSpeed = 7f;
@@ -18,26 +17,27 @@ public class PlayerMovement : MonoBehaviour
     float animspeed = 1.9f;
     float _rotationAmount;
     Quaternion _turnOffset; bool _jumping = false;
+    public AudioClip[] footStepSounds;
+
+
+    CapsuleCollider collider;
 
     private Animator _animator;
     void Start()
     {
+        collider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         _animator = GetComponent<Animator>();
-        Debug.Log(_animator);
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            _jumping = false;
-        }
     }
 
     private void OnFootstep(AnimationEvent animationEvent)
     {
-
+        if (animationEvent.animatorClipInfo.weight > 0.5f)
+        {
+            var index = Random.Range(0, footStepSounds.Length);
+            AudioSource.PlayClipAtPoint(footStepSounds[index], transform.TransformPoint(collider.center));
+        }
     }
 
     void FixedUpdate()
@@ -48,10 +48,6 @@ public class PlayerMovement : MonoBehaviour
 
         _moveSideways = Vector3.zero; _rotationAmount = _moveH * _rotationSpeed * Time.deltaTime; _turnOffset = Quaternion.Euler(0, _rotationAmount, 0); rb.MoveRotation(rb.rotation * _turnOffset);
 
-        if (rb.position.y < -25)
-        {
-            rb.position = new Vector3(-11, 3, 0);
-        }
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
